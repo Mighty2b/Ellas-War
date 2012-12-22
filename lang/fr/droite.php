7b0e8bc5590dcb21f430688710fbe25e58cbfe05
Mighty <mighty@ellaswar.com>
2012-12-22 14:36:51 +0100

    Fin du design de noel

---
+++ lang/fr/droite.php
@@ -0,0 +1,19 @@
+<div style="position:relative;width:88px;height:31px;margin-left:35px;margin-bottom:3px;"><a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/" target="_blank"><img src="http://www.jeu-gratuit.net/images/bannieres/88x31_general_1.png" alt="jeux gratuits : Jeux de stratégie & MMORPG"  width="88" height="31" border="0" /></a><a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/#jeux" target="_blank" style="position:absolute; top:3px; left:1px; font-family:arial; font-size:11px; font-weight:bold; color:#ffffff; text-decoration:none; line-height:12px; width:86px; text-align:center;">JDR en ligne<br />
+mmorpg</a></div>
+		
+		<a href="http://www.jeu-gratuit.net/#jeux" target="_blank"><img src="http://www.jeu-gratuit.net/images/bouton88x31.png" alt="Jeux" /></a>
+
+<script type="text/javascript">
+    micropolia_echban_id      = 412;
+    micropolia_echban_width   = 88;
+    micropolia_echban_height  = 31;
+    micropolia_echban_jeu     = 11221;
+    micropolia_echban_proprio = 342;
+    micropolia_echban_theme   = 2;
+</script>
+<script src="http://www.jeu-gratuit.net/tracking/affiche.js" type="text/javascript"></script>
+<br/>
+<a href="http://www.argent-gratuit.net" target="_blank"><img src="http://www.argent-gratuit.net/bouton.gif" width="88" height="31" alt="argent-gratuit"/></a>
+<br/>
+<a href="http://www.cadomax.com" target="_blank" ><img src="http://www.cadomax.com/design/8831-cadomax-01.gif" border="0" width="88" height="31" alt="jeux" title="jeux"></a>
+
---
+++ lang/fr/gauche.php
@@ -0,0 +1,18 @@
+<div style="position:relative;width:88px;height:31px;margin-left:35px;margin-bottom:3px;">
+<a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/jeu-ellas-war.html" target="_blank"><img src="http://www.jeu-gratuit.net/images/bannieres/88x31_general_1.png" alt="votez pour Ellas War" title="votez pour Ellas War" width="88" height="31" border="0" /></a>
+<a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/jeu-ellas-war.html#voter" target="_blank" style="position:absolute; top:8px; left:-1px; font-family:arial; font-size:11px; font-weight:bold; color:#ffffff; text-decoration:none; line-height:12px; width:90px; text-align:center;" title="votez pour Ellas War">Votez ici !!</a></div>
+
+<script type="text/javascript"><!--
+var baniziuri = "http://s2.banizi.com/show.aspx?zid=2659&rnd=" + new String (Math.random()).substring (2, 8);
+document.write("<scr"+"ipt type=text/javascript src="+baniziuri+"></scr"+"ipt>");
+//-->
+</script>
+
+<script type="text/javascript" src="http://exchange.pbem.fr/view.php?id=73&amp;f=2"></script> 
+<br/>
+<a href="http://www.clicjeux.net" target="_blank"><img src="http://www.clicjeux.net/images/bouton.gif" alt="clicjeux.net" border="0"></a>
+<br/>
+<a href="http://www.medieval-war.com/index.php?id=65" target="_blank"><img src="http://www.medieval-war.com/banniere2.gif" alt="medieval-war"/></a>
+<br/>
+<!--<a href="http://www.lesjeuxgratos.com" title="Les jeux Gratos" target="_blank"><img src="http://www.lesjeuxgratos.com/images/banniere88.jpg" alt="Jeux en ligne gratuits" /></a>-->
+<br/>
