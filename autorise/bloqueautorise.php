7d7a1b41baaa6578f2d88c9c584d96fc15be2985
Mighty <mighty@ellaswar.com>
2013-01-02 17:20:53 +0100

    Les tours de force sont visibles pour les visiteurs

--- autorise/bloqueautorise.php
+++ autorise/bloqueautorise.php
@@ -1,5 +1,14 @@
 <?php
 
-	$autorise = array('accueil_bloque', 'decouvertedujeu', 'faq', 'pagecontact', 'partenaires', 'conditionsgeneralesutilisation', 'quelquesmotssurlesite', 'equipedusite', 'plandusite', 'recrutement', 'premierspas', 'lesattaques', 'lestemples', 'generalitesalliances', 'lesespionnages', 'lesguerresetpactes', 'lemarche', 'lesressources', 'lesfaveurs', 'questionsfrequentes', 'quelquesregles', 'classementdesjoueurs', 'classementdesalliances', 'classements', 'profilsjoueur', 'profilsalliance', 'membrealliance', 'joueursconnectes', 'erreur', 'permalien', 'actualites', 'soutien', 'teamspeak', 'allianceexistepas', 'jeu', 'animations');
+$autorise = array(
+	'accueil_bloque', 'decouvertedujeu', 'faq', 'pagecontact', 'partenaires', 
+	'conditionsgeneralesutilisation', 'quelquesmotssurlesite', 'equipedusite', 
+	'plandusite', 'recrutement', 'premierspas', 'lesattaques', 'lestemples', 
+	'generalitesalliances', 'lesespionnages', 'lesguerresetpactes', 'lemarche', 
+	'lesressources', 'lesfaveurs', 'questionsfrequentes', 'quelquesregles', 
+	'classementdesjoueurs', 'classementdesalliances', 'classements', 
+	'profilsjoueur', 'profilsalliance', 'membrealliance', 'joueursconnectes', 
+	'erreur', 'permalien', 'actualites', 'soutien', 'teamspeak', 
+	'allianceexistepas', 'jeu', 'animations', 'tour_de_force');
 
 ?>
--- autorise/pauseautorise.php
+++ autorise/pauseautorise.php
@@ -1,3 +1,15 @@
 <?php
-	$autorise = array('accueil_pause', 'decouvertedujeu', 'faq', 'pagecontact', 'partenaires', 'conditionsgeneralesutilisation', 'quelquesmotssurlesite', 'equipedusite', 'plandusite', 'recrutement', 'premierspas', 'lesattaques', 'lestemples', 'generalitesalliances', 'lesespionnages', 'lesguerresetpactes', 'lemarche', 'lesressources', 'lesfaveurs', 'questionsfrequentes', 'quelquesregles', 'classementdesjoueurs', 'classementdesalliances', 'classements', 'profilsjoueur', 'profilsalliance', 'membrealliance', 'joueursconnectes', 'erreur', 'obtenirdesfaveurs', 'faveurerreur', 'faveurvalide', 'permalien', 'actualites', 'soutien', 'teamspeak', 'allianceexistepas', 'jeu', 'animations', 'valide');
+
+$autorise = array(
+	'accueil_pause', 'decouvertedujeu', 'faq', 'pagecontact', 'partenaires', 
+	'conditionsgeneralesutilisation', 'quelquesmotssurlesite', 'equipedusite', 
+	'plandusite', 'recrutement', 'premierspas', 'lesattaques', 'lestemples', 
+	'generalitesalliances', 'lesespionnages', 'lesguerresetpactes', 
+	'lemarche', 'lesressources', 'lesfaveurs', 'questionsfrequentes', 
+	'quelquesregles', 'classementdesjoueurs', 'classementdesalliances', 
+	'classements', 'profilsjoueur', 'profilsalliance', 'membrealliance', 
+	'joueursconnectes', 'erreur', 'obtenirdesfaveurs', 'faveurerreur', 
+	'faveurvalide', 'permalien', 'actualites', 'soutien', 'teamspeak', 
+	'allianceexistepas', 'jeu', 'animations', 'valide', 'tour_de_force');
+
 ?>
