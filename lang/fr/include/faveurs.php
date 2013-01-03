9e311e7d747b3e4d2d19cda681fec02d72296e77
Mighty <mighty@ellaswar.com>
2013-01-03 20:09:44 +0100

    Demande de validation pour les achats

--- lang/fr/include/faveurs.php
+++ lang/fr/include/faveurs.php
@@ -12,21 +12,39 @@ echo '
 if($paquet->getlvl() >= LVL_MINI_XP) {
   echo '
   <div class="cadre_renta centrer">
-    <a href="Faveurs-zeus"><b>Appui de Zeus</b></a><br/>
-    <br/><a href="Faveurs-zeus">Appui de Déimos</a>
-    <br/><a href="Faveurs-zeus">Appui d\'Éros</a>
-    <br/><a href="Faveurs-zeus">Appui d\'Hébé</a>
+    <a href="Faveurs-zeus" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui de Zeus</b></a><br/>
+    <br/><a href="Faveurs-zeus" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Appui de Déimos</a>
+    <br/><a href="Faveurs-zeus" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Appui d\'Éros</a>
+    <br/><a href="Faveurs-zeus" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Appui d\'Hébé</a>
     <br/>
     <br/>Prix : 2 faveurs
   </div>';
 }
 echo '
   <div class="cadre_renta centrer">
-    <a href="Faveurs-hermes"><b>Appui d\'Hermès</b></a><br/>
-    <br/><a href="Faveurs-hermes">+50% de lots sur le commerce</a>
-    <br/><a href="Faveurs-hermes">-50% sur la taxe lors des rachats</a>
-    <br/><a href="Faveurs-hermes">Licence de grand commerçant</a>
-    <br/><a href="Faveurs-hermes">Ventes/Achats anonymes sans frais</a>
+    <a href="Faveurs-hermes" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui d\'Hermès</b></a><br/>
+    <br/><a href="Faveurs-hermes" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+50% de lots sur le commerce</a>
+    <br/><a href="Faveurs-hermes" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >-50% sur la taxe lors des rachats</a>
+    <br/><a href="Faveurs-hermes" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Licence de grand commerçant</a>
+    <br/><a href="Faveurs-hermes" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Ventes/Achats anonymes sans frais</a>
     <br/>
     <br/>Prix : 2 faveurs pour une semaine
   </div>';
@@ -37,18 +55,32 @@ if($paquet->getlvl() >= LVL_MINI_XP) {
 }
 echo '
   <div class="cadre_renta centrer">
-    <a href="Faveurs-deimos"><b>Appui de Déimos</b></a><br/>
-    <br/><a href="Faveurs-deimos">Remportez de nombreuses victoires !</a>
-    <br/><a href="Faveurs-deimos">15 attaques bonus vous sont ajoutées. Vous pouvez en faire un maximum de 5 par jour.</a>
+    <a href="Faveurs-deimos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui de Déimos</b></a><br/>
+    <br/><a href="Faveurs-deimos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Remportez de nombreuses victoires !</a>
+    <br/><a href="Faveurs-deimos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >15 attaques bonus vous sont ajoutées. Vous pouvez en faire un maximum de 5 par jour.</a>
     <br/>
     <br/>Prix : 1 faveur
   </div>';
 if($paquet->getlvl() >= LVL_MINI_XP) {
   echo '<div class="cadre_renta centrer">
-    <a href="Faveurs-eros"><b>Appui d\'Éros</b></a><br/><br/>
-    <a href="Faveurs-eros">Augmentez votre XP pendant une semaine !</a>
-    <br/><a href="Faveurs-eros">- 20% de perte d\'XP lors d\'une défaite</a>
-    <br/><a href="Faveurs-eros">+ 20% d\'XP en cas de victoire</a>
+    <a href="Faveurs-eros" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui d\'Éros</b></a><br/><br/>
+    <a href="Faveurs-eros" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Augmentez votre XP pendant une semaine !</a>
+    <br/><a href="Faveurs-eros" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >- 20% de perte d\'XP lors d\'une défaite</a>
+    <br/><a href="Faveurs-eros" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+ 20% d\'XP en cas de victoire</a>
     <br/>
     <br/>Prix : 1 faveur
   </div>';
@@ -57,33 +89,55 @@ echo '
 </div>
 <div class="ligne">
   <div class="cadre_renta centrer">
-    <a href="Faveurs-midas"><b>Appui de Midas</b></a><br/><br/>
-    <a href="Faveurs-midas">Augmentez vos richesses !</a>
-   <br/><a href="Faveurs-midas">+ 800\'000 Drachmes</a>
+    <a href="Faveurs-midas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui de Midas</b></a><br/><br/>
+    <a href="Faveurs-midas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Augmentez vos richesses !</a>
+   <br/><a href="Faveurs-midas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+ 800\'000 Drachmes</a>
    <br/>
    <br/>Prix : 1 faveur
   </div>
 
   <div class="cadre_renta centrer">
-    <a href="Faveurs-ploutos"><b>Appui de Ploutos</b></a><br/><br/>
-    <a href="Faveurs-ploutos">Ayez des ressources en abondance !</a>
-    <br/><a href="Faveurs-ploutos">+ 2\'000\'000 Bois, Fer, Nourriture, Eau et 1\'000\'000 d\'Argent</a>
+    <a href="Faveurs-ploutos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui de Ploutos</b></a><br/><br/>
+    <a href="Faveurs-ploutos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Ayez des ressources en abondance !</a>
+    <br/><a href="Faveurs-ploutos" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+ 2\'000\'000 Bois, Fer, Nourriture, Eau et 1\'000\'000 d\'Argent</a>
     <br/>
     <br/>Prix : 1 faveur
   </div>
 </div>
 <div class="ligne">
   <div class="cadre_renta centrer">
-    <a href="Faveurs-hebe"><b>Appui d\'Hébé</b></a><br/><br/>
-    <a href="Faveurs-hebe">Vous ne payerez pas de taxes lors de vos retraits dans votre trésor durant 7 jours</a>
+    <a href="Faveurs-hebe" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui d\'Hébé</b></a><br/><br/>
+    <a href="Faveurs-hebe" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >Vous ne payerez pas de taxes lors de vos retraits dans votre trésor durant 7 jours</a>
     <br/>
     <br/>Prix : 1 faveur
   </div>
 
   <div class="cadre_renta centrer">
-  <a href="Faveurs-leonnidas"><b>Appui de Léonidas</b></a><br/>
-  <br/><a href="Faveurs-leonnidas">+ 40 Spartiates</a>
-  <br/><a href="Faveurs-leonnidas">+ 200 espions</a>
+  <a href="Faveurs-leonnidas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       ><b>Appui de Léonidas</b></a><br/>
+  <br/><a href="Faveurs-leonnidas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+ 40 Spartiates</a>
+  <br/><a href="Faveurs-leonnidas" 
+       onclick="if (window.confirm(\'Valider votre achat ?\')) { this.disabled=\'true\';} else { return false; }"
+       >+ 200 espions</a>
   <br/>
   <br/>Prix : 1 faveur
   </div>
