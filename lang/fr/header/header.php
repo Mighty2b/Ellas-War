78dca53d1a1e706b95306593f0e8a6c2052a5362
Mighty <mighty@ellaswar.com>
2013-01-08 11:57:07 +0100

    meta langue

--- lang/fr/header/header.php
+++ lang/fr/header/header.php
@@ -1,2 +1,3 @@
 	<meta name="keywords" content="jeux , strategie , guerre , gratuit , ligne, ellas, hellas, grece, war, dieux, antique, online, forum, communauté, peuples, antiquité, grecque, cité, armée, maître, civilisation, poseidon, zeus, hades, temple"/>
+	<meta http-equiv="content-language" content="fr">
 	
