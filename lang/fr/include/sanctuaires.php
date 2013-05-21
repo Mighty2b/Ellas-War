68c5f155078b2c0e7635771bce5d3bf23d8dbc96
Mighty <mighty@ellaswar.com>
2013-05-21 19:27:46 +0200

    Affichage des sanctuaires pour les admins et non affichage des sanctuaires vides

--- lang/fr/include/sanctuaires.php
+++ lang/fr/include/sanctuaires.php
@@ -12,40 +12,42 @@ echo '
 echo '<div class="ligne80">';
 
 echo '<h1>Sanctuaires</h1>';
-	if($paquet->getalliance() == 0) {
+	if($paquet->getalliance() == 0 && $paquet->getlvl2() < 3) {
 		echo '<div class="erreur centrer">Vous devez être dans une alliance pour accéder aux sanctaires.</div>';
 	}
 	else {
 	
 	foreach($liste_sanctuaires as $sanctuaire) {
-		echo '<div class="gauche"><b>'.$sanctuaire->nom.'</b><br/>';
-		echo stripslashes($sanctuaire->description).'<br/><br/>';
-	
-		echo '<b>Ennemis :</b><br/>
-					<ul>';
-		foreach($sanctuaire->mb as $membre) {
-			echo '<li>'.$membre->nom.'</li>';
-		}
-		echo '</ul>';
+		if(sizeof($sanctuaire->mb) > 0) {
+			echo '<div class="gauche"><b>'.$sanctuaire->nom.'</b><br/>';
+			echo stripslashes($sanctuaire->description).'<br/><br/>';
 		
-		if(!empty($sanctuaire_actuel)) {
-			if($sanctuaire->id == $sanctuaire_actuel) {
-				echo '<div class="ligne">
-							<a onclick="javascript:constituer_groupe('.$sanctuaire->id.');"
-								 class="details_ress img_temple">Continuer votre sanctuaire</a>
-							</div></div>';
+			echo '<b>Ennemis :</b><br/>
+						<ul>';
+			foreach($sanctuaire->mb as $membre) {
+				echo '<li>'.$membre->nom.'</li>';
+			}
+			echo '</ul>';
+			
+			if(!empty($sanctuaire_actuel)) {
+				if($sanctuaire->id == $sanctuaire_actuel) {
+					echo '<div class="ligne">
+								<a onclick="javascript:constituer_groupe('.$sanctuaire->id.');"
+									 class="details_ress img_temple">Continuer votre sanctuaire</a>
+								</div></div>';
+				}
+				else {
+					echo '<div class="ligne">
+								Un sanctuaire est déjà en cours
+								</div></div>';
+				}
 			}
 			else {
 				echo '<div class="ligne">
-							Un sanctuaire est déjà en cours
+							<a onclick="javascript:constituer_groupe('.$sanctuaire->id.');"
+								 class="details_ress img_temple">Constituer un groupe</a>
 							</div></div>';
 			}
-		}
-		else {
-			echo '<div class="ligne">
-						<a onclick="javascript:constituer_groupe('.$sanctuaire->id.');"
-							 class="details_ress img_temple">Constituer un groupe</a>
-						</div></div>';
 		}
 	}
 }
