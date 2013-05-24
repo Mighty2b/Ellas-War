a511bf324e091a7c7fe3894cd8046bce7f7d1489
Mighty <mighty@ellaswar.com>
2013-05-24 13:51:09 +0200

    Tout le commerce sur une page

--- lang/fr/menu_marche.php
+++ lang/fr/menu_marche.php
@@ -1,7 +1,8 @@
 <div id="menu_haut_marche" class="menuh_visible">
-	<div id="bouton_haut_donner"><a href="DonnerUneFaveur"><img src="design/co/menu_haut_donner.png" alt="Donner une faveur" title="Donner une faveur" /></a></div>
-	<div id="bouton_haut_details"><a href="MarcheAuDetails"><img src="design/co/menu_haut_details.png" alt="Marché au détails" title="Marché au détails" /></a></div>
-	<div id="bouton_haut_gros"><a href="MarcheDeGros"><img src="design/co/menu_haut_gros.png" alt="Marché de gros" title="Marché de gros" /></a></div>
+	<div id="bouton_haut_donner"><a href="DonnerUneFaveur"><img src="design/co/menu_haut_donner2.png" alt="Donner une faveur" title="Donner une faveur" /></a></div>	
+	<div id="bouton_haut_marche"><a href="Marche"><img src="design/co/menu_haut_marchee.png" alt="Marché" title="Marché" /></a></div>
+<!-- <div id="bouton_haut_details"><a href="MarcheAuDetails"><img src="design/co/menu_haut_details.png" alt="Marché au détails" title="Marché au détails" /></a></div> -->	
+<!-- <div id="bouton_haut_gros"><a href="MarcheDeGros"><img src="design/co/menu_haut_gros.png" alt="Marché de gros" title="Marché de gros" /></a></div> -->
 	<div id="bouton_haut_vendre"><a href="Vendre"><img src="design/co/menu_haut_vendre.png" alt="Vendre" title="Vendre" /></a></div>
 	<div id="bouton_haut_licences"><a href="Licences"><img src="design/co/menu_haut_licences.png" alt="Licences" title="Licences" /></a></div>
 	<div id="bouton_haut_ventes"><a href="MesVentes"><img src="design/co/menu_haut_ventes.png" alt="Mes ventes" title="Mes ventes" /></a></div>
