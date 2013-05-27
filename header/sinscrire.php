c79ea9b7c8cd96618883e676d271bfa9c83f35b7
Mighty <mighty@ellaswar.com>
2013-05-27 20:48:07 +0200

    Modification du script du parrainage + image du commit precedent

--- header/sinscrire.php
+++ header/sinscrire.php
@@ -7,12 +7,16 @@ if(!empty($_GET['var1'])) {
 
 if(!empty($_POST['Inscription'])) {
 	if(empty($_SESSION['parrain'])) {
-		$_SESSION['parrain'] = '';
+		$paquet = new EwPaquet('inscription',
+		                       array($_POST['pseudo'], $_POST['pass'],
+		                             $_POST['pass2'], $_POST['mail'],0));
 	}
-	$paquet = new EwPaquet('inscription',
-												 array($_POST['pseudo'], $_POST['pass'],
-												 			 $_POST['pass2'], $_POST['mail'],
-												 			 $_SESSION['parrain']));
+	else {
+		$paquet = new EwPaquet('inscription',
+		                       array($_POST['pseudo'], $_POST['pass'],
+		                       $_POST['pass2'], $_POST['mail'],
+		                       $_SESSION['parrain']->id));
+	}
 	if($paquet->getstatu() == 1) {
 		redirect();
 	}
