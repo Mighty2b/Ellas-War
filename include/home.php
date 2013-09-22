6476dce8316ba4a1716d0df56ec4834571ee5890
Mighty <mightykiller2b@gmail.com>
2013-09-22 22:22:21 +0200

    l'accueil est lisible

--- include/home.php
+++ include/home.php
@@ -1,3 +1,5 @@
+<?php
+	echo '
 <section id="home">
 	<div class="ligne">
 	<img src="images/design_deco/Ellaswarheader.png" 
@@ -14,7 +16,34 @@
 		<br/><br/>
 	</section>
 	<section id="inside">
-		<nav>Navigation</nav>
-		<article>Intérieur</article>
+		<article>
+<h1>'._('Venez combattre sur Ellàs War').'</h1>
+<p>'._('Inscrivez-vous gratuitement sur Ellàs War, '.
+'affrontez des milliers de joueurs et '.
+'défendez votre cité face aux monstres qui l\'entourent. ').'</p>
+<h2>'._('Déployez votre stratégie').'</h2>
+<p>'._('Bâtissez votre cité au milieu d\'une Grèce à feu et à sang. '.
+'Enrôlez des hommes et construisez des défenses. '.
+'Engagez des forces, faites vos propres stratégies et '.
+'partez à l\'assaut des autres cités. '.
+'Ou préférerez-vous peut-être développer vos productions et '.
+'faire prospérer votre cité économiquement grâce au commerce ? ').'</p>
+<h2>'._('Faites intervenir les dieux').'</h2>
+<p>'._('Honorez les dieux, vivez en les respectant et en les craignant. '.
+'Vous devrez faire des choix, chacun vous donnera un avantage unique. '.
+'Ce sont eux qui apporteront à votre cité gloire et renommée ou '.
+'au contraire la conduiront à sa perte. '.
+'N\'oubliez pas, les dieux sont parmi vous, ils vous observent. ').'</p>
+<h2>'._('Chaque avantage compte').'</h2>
+<p>'._('L\'arbre des dieux vous permettra de spécialiser votre cité. '.
+'Vénérez les dieux et les grands hommes de la Grèce antique '.
+'grâce aux autels et aux statues et ils vous en remercieront. ').'</p>
+<h2>'._('Prenez des risques').'</h2>
+<p>'._('Faites des stocks grâce à votre trésor ou dépensez tout afin d\'améliorer vos soldats. '.
+'N\'oubliez pas que chaque choix peut avoir des conséquences. '.
+'Mais ne faut-il pas prendre des risques '.
+'pour devenir la plus puissante des cités Grecques ? ').'</p></article>
 	</section>
-</section>
+</section>';
+
+?>
