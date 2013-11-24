fd0f8cb3f1cb2186244aea0e2978716e1021512e
Mighty <mightykiller2b@gmail.com>
2013-11-24 17:01:06 +0100

    api get_news

---
+++ header/actualites.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<title>'._('Actualités d\'Ellàs War').'</title>
+<meta name="description" content="'._('Les actualités d\'Ellàs War.').'" />';
+
+$paquet -> add_action('get_news', array(20));
+
+?>
