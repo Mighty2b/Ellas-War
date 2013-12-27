0d8315304f6617f630563c2a1e746729f91eb0b2
Mighty2b <mighty@ellaswar.com>
2013-12-27 11:55:48 +0100

    Page des actualités du jeu

--- include/actualites.php
+++ include/actualites.php
@@ -5,18 +5,16 @@ $list_news = $paquet->get_answer('get_news')->{1};
 echo '<h1 class="title_faq">'._('Actualités d\'Ellàs War').'</h1>';
 
 if(sizeof($list_news) > 0) {
-
-	echo '<br/>
-	<table class="centrer_tableau">';
-
+	echo '<div id="new_list" class="ligne">
+	<h2>'._('News').'</h2>';
+	
 	foreach($list_news as $i => $news) {
-		echo '
-<tr>
-		<td><a href="'.$news->lien.'" class="titre_news" target="_blank" >'.$news->titre.'</a>, <i>'.display_date($news->temps, 1).'</i> - '._('Par').' '.$news->auteur.'</td>
-</tr>';
+		echo '<div class="ligne">
+			<a href="'.$news->lien.'" class="titre_news" target="_blank" >'.$news->titre.'</a>, <i>'.display_date($news->temps, 1).'</i></td>
+		</div>';
 	}
 	
-	echo '</table>';
+	echo '</div>';
 }
 
 ?>
