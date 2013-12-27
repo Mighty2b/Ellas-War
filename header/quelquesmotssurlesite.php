a9a1af5ee5592621b8d81f649684cef42739e30a
Mighty2b <mighty@ellaswar.com>
2013-12-27 14:03:42 +0100

    Quelques mots sur EW

---
+++ header/quelquesmotssurlesite.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<title>'._('Quelques mot sur EllasWar.com Jeu de stratégie gratuit sur le theme de l\'antiquité grecque').'</title>';
+echo '<meta name="description" content="'._('Quelques mot sur EllasWar.com, jeu de stratégie en ligne gratuit au temps de l\'antiquité grecque.').
+      '" />';
+?>
---
+++ include/quelquesmotssurlesite.php
@@ -0,0 +1,43 @@
+<?php 
+
+echo '<h1>'._('Quelques mots sur le site').'</h1>';
+
+echo '
+<p><h2>'._('Présentation d\'Ellàs War').'</h2>
+<div class="ligne ligne80 justify">'._(
+'Ellàr War est un <a href="'.SITE_URL.'">jeu gratuit de stratégie en ligne</a> '.
+'par navigateur. L\'action se déroule environ vers -500 durant l\'age d\'or '.
+'de la Grèce Antique. À sa sortie en avril 2007, il s\'agissait d\'un projet '.
+'personnel, peut à peut l\'équipe du site s\'est renforcée et ses compétences '.
+'se sont étendues. Celui-ci a évolué au cours du temps aussi bien graphiquement '.
+'que techniquement. Le jeu est en développement constant et des mises à jours '.
+'sont ajoutées régulièrement.').
+'<br/><br/></div></p>
+
+<p><h2>'._('Services Annexes').'</h2>
+<div class="ligne ligne80 justify">'._(
+'En plus du site, Ellàs War possède aussi '.
+'<a href="'.FORUM_URL.'" target="_blank">un forum</a> et '.
+'<a href="'.WIKI_URL.'" target="_blank">un wiki</a>. '.
+'Sur <a href="'.FORUM_URL.'">le forum</a> vous pourrez échanger avec le reste '.
+'de la communauté et le staff. Il est animé et modéré par '.
+'<a href="'.SITE_URL.'/equipedusite">l\'équipe du jeu</a>, '.
+'vous pourrez y trouver des informations et poser vos questions. '.
+'Ellàs War possède aussi un Wiki où vous pourrez trouver toutes les '.
+'informations dont vous aurez besoin. Si une information n\'est pas '.
+'disponible sur le Wiki et qu\'elles ne fait pas partie des secrets du jeu, '.
+'vous pouvez l\'ajouter ou en faire la demande sur le forum.').
+'<br/><br/></div></p>
+
+<p><h2>'._('L\'équipe du jeu').'</h2>
+<div class="ligne ligne80 justify">'._(
+'<a href="'.SITE_URL.'/equipedusite">L\'équipe d\'Ellàs War</a> est constituée '.
+'par les administrateurs à partir des membres du jeu. Celle-ci est bénévole '.
+'et fait tout son possible pour permettre à tous les joueurs de s\'amuser '.
+'tout en gardant une bonne ambiance. Elle est bénévole, l\'argent récolté via '.
+'les faveurs sert au paiement de l\'hébergement et des différents noms de domaine. '.
+'Les différents services payant fournis par le jeu peuvent aussi à financer '.
+'des projets annexes comme le serveur de sauvegarde et des sites liés à Ellàs War.').
+'<br/><br/></div></p>';
+
+?>
