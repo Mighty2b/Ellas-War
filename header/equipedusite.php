237f588ca880f9f9dbaf3c76c4bbb5ba0eb85109
Mighty2b <mighty@ellaswar.com>
2013-12-27 15:12:36 +0100

    Page de l'équipe

---
+++ header/equipedusite.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<title>'._('L\'équipe d\'Ellàs War').'</title>
+<meta name="description" content="'._('L\'équipe d\'Ellàs War, vous y trouverez toutes les informations sur le jeu.').'" />';
+
+?>
