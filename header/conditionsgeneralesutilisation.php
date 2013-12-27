60086fb0fc47643f427b141dc588a6396d109175
Mighty2b <mighty@ellaswar.com>
2013-12-27 15:17:56 +0100

    Header CGU

---
+++ header/conditionsgeneralesutilisation.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<title>'._('Conditions générales d\'utilisation d\'EllasWar').'</title>
+<meta name="description" content="'._('Conditions générales d\'utilisation d\'EllasWar').'" />';
+
+?>
