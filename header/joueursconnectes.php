9e1e29922f503c41dcf54d15acf09c9eb9756131
Mighty2b <mighty@ellaswar.com>
2013-12-27 13:31:36 +0100

    Mise en forme tableaux + joueurs co

--- header/joueursconnectes.php
+++ header/joueursconnectes.php
@@ -1,5 +1,8 @@
 <?php
 
+echo '<title>'._('Joueurs connectés').'</title>';
+echo '<meta name="description" content="'._('Joueurs connectés sur Ellàs War').'" />';
+
 $paquet -> add_action('get_joueurs_co');
 
 ?>
---
+++ include/joueursconnectes.php
@@ -0,0 +1,44 @@
+<?php
+
+$liste = $paquet->get_answer('get_joueurs_co')->{1};
+
+echo '<h1>'._('Joueurs connectés').'</h1>
+<br/>
+<table class="centrer_tableau">
+	<thead>
+	<tr class=\'titre_tab\'>
+		<td>'._('Joueur').'</td>
+		<td>'._('Niveau').'</td>
+		<td style="min-width:60px;">'._('Terrain').'</td>
+		<td style="min-width:100px;" class="gauche">'._('Alliance').'</td>
+	</tr>
+	</thead>
+	<tfoot></tfoot>
+	<tbody>';
+
+foreach($liste as $do) {
+	echo'<tr>
+	<td>&nbsp;<a href=\'/'.trad_to_page('profilsjoueur').'-'.$do->id.'\'>'.$do->login.'</a> ';
+	
+	if($do->id == $do->chef) {
+		echo img('images/joueurs/mini_laurier.png', _('Grand chef'));
+	}
+	
+	echo '&nbsp;</td>
+	<td align=\'right\'>&nbsp;'.$do->lvl.'&nbsp;</td>
+	<td align=\'right\'>&nbsp;'.nbf($do->terrain).'&nbsp;</td>
+	<td>&nbsp;';
+	if(!empty($do->alliance)) {
+		echo '<a href="'.trad_to_page('profilsalliance').'-'.$do->alliance.'">'.$do->nom.'</a>';
+	}
+	else {
+		echo $do->nom;
+	}
+	echo '&nbsp;</td></tr>';
+}
+
+echo '</tbody>
+</table>
+<br/>';
+
+?>
