7c5ff7e6880b8e9b3f8cab1d4a5c45ecd61ee86f
Mighty2b <mighty@ellaswar.com>
2013-12-29 07:22:55 +0100

    Fix warning pour dev hors ligne

--- include/actualites.php
+++ include/actualites.php
@@ -1,19 +1,21 @@
 <?php
 
-$list_news = $paquet->get_answer('get_news')->{1};
-
 echo '<h1 class="title_faq">'._('Actualités d\'Ellàs War').'</h1><br/>';
 
-if(sizeof($list_news) > 0) {
-	echo '<div id="new_list" class="ligne">';
+if(!empty($paquet->get_answer('get_news'))) {
+	$list_news = $paquet->get_answer('get_news')->{1};
 	
-	foreach($list_news as $i => $news) {
-		echo '<div class="ligne">
-			<a href="'.$news->lien.'" class="titre_news" target="_blank" >'.$news->titre.'</a>, <i>'.display_date($news->temps, 1).'</i></td>
-		</div>';
+	if(sizeof($list_news) > 0) {
+		echo '<div id="new_list" class="ligne">';
+		
+		foreach($list_news as $i => $news) {
+			echo '<div class="ligne">
+				<a href="'.$news->lien.'" class="titre_news" target="_blank" >'.$news->titre.'</a>, <i>'.display_date($news->temps, 1).'</i></td>
+			</div>';
+		}
+		
+		echo '<div class="ligne"><br/></div></div>';
 	}
-	
-	echo '<div class="ligne"><br/></div></div>';
 }
 
 ?>
