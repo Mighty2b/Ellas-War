ab541b3c0a46983f4227d98cf801cf4747e4e51b
Mighty2b <mighty@ellaswar.com>
2013-12-29 21:28:24 +0100

    Page s'inscrire

---
+++ include/sinscrire.php
@@ -0,0 +1,24 @@
+<?php
+echo '<h1>'._('Inscription sur Ellàs War').'</h1>';
+
+echo '<p><br/></p>';
+
+echo '<p>'._(
+'Le formulaire d\'inscription se trouve dans le cadre situé '.
+'à droite de la page. En vous inscrivant, vous acceptez le règlement du jeu. '.
+'Nous vous conseillons de ne pas utiliser de caractères spéciaux dans votre '.
+'identifiant.').
+'</p><p><br/></p><p>'.
+_('Lors de votre inscription, l\'adresse email renseignée doit être valide. '.
+'L\'équipe d\'Ellàs War pourra être amenée à l\'utiliser pour vous contacter').
+'</p><p><br/></p><p>'.
+_('Nous vous rappellons que l\'utilisation de plusieurs comptes par une même '.
+'personne est strictement interdite.').'</p>';
+
+echo '<div id="sinscrire" class="centrer">
+<img src="images/illustration/inscription.png"
+     alt="'._('S\'inscrire').'" 
+     title="'._('S\'inscrire').'" />
+</div>';
+
+?>
