65eddb02a46b8d96036e79a959be662ba653b7c8
Mighty2b <mighty@ellaswar.com>
2013-12-29 07:46:35 +0100

    Header partenaires

---
+++ header/partenaires.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<title>'._('Nos partenaires').'</title>
+<meta name="description" content="'._('Nos partenaires. Si vous désirez obtenir un partenaria avec Ellas War et voir votre bannière affichée sur le site, envoyez nous un message par la page contact. Présentez-nous votre site et dites-nous pourquoi vous désirez ce partenariat.').'" />';
+
+?>
