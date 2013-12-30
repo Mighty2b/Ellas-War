b1aa2928f43c6ade149578781354e1ca3d6db47f
Mighty2b <mighty@ellaswar.com>
2013-12-30 09:07:30 +0100

    Page partenaires + illustration inscription

---
+++ include/partenaires.php
@@ -0,0 +1,22 @@
+<?php
+
+echo '<h1>'._('Partenaires').'</h1>
+<br/>
+<p>'._(
+'Vous désirez obtenir un partenarial avec Ellas War et voir '.
+'votre bannière affichée sur le site, envoyez nous un message '.
+'par la page contact. Présentez-nous votre site et dites-nous '.
+'pourquoi vous désirez ce partenariat.').'<br/><br/></p>
+
+<div class="centrer">
+<img src="banniere.gif" alt="Ellàs War" title="Ellàs War" />
+<textarea class="part_banniere"><a href="'.SITE_URL.'" target="_blank"><img src="'.SITE_URL.'/banniere.gif" alt="Ellàs War" /></a></textarea>
+</div>
+<br/>
+<div class="centrer">
+<img src="banmini.gif" alt="Ellàs War" title="Ellàs War" /><br/>
+<textarea class="part_banniere"><a href="'.SITE_URL.'" target="_blank"><img src="'.SITE_URL.'/banmini.gif" alt="Ellàs War" /></a></textarea>';
+
+//TODO : Mettre les partenaires
+
+?>
