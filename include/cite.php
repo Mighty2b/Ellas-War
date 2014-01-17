1198e049c28c760fc606c495f575b20fbcb7b39e
Mighty <mightykiller2b@gmail.com>
2014-01-18 00:41:43 +0100

    traduction page cité

--- include/cite.php
+++ include/cite.php
@@ -92,69 +92,71 @@ if(!empty($liste_autels)) {
 	$niveaux_arbres += $liste_autels->{'sauvegarde_ombre'};
 }
 
-?>
+echo '
 <div id="info_cite_gauche">
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_cite">
-	<div class="ligne centrer"><a href="constructions" class="titre">Cité</a></div>
-	<div class="ligne"><a class="mtitre" href="constructions">Bâtiments</a></div>
+	<div class="ligne centrer"><a href="'._('constructions').'" class="titre">'._('Cité').'</a></div>
+	<div class="ligne"><a class="mtitre" href="'._('constructions').'">'._('Bâtiments').'</a></div>
 	<div class="ligne">
 		<table width="100%" class="none">
 		<thead></thead>
 		<tfoot></tfoot>
 		<tbody>
 		<tr>
-			<td width="50%"><a href="Construire-1"><b>Battage de la monnaie :</b> <?php echo nbf($constructions->atelierf->nb); ?></a></td> 
-			<td width="50%"><a href="Construire-1"><b>Production : </b> <?php echo nbf($batis[0]-$constructions->atelierf->nb); ?></a></td>
+			<td width="50%"><a href="'._('construire').'-1"><b>'._('Battage de la monnaie').' :</b> '.nbf($constructions->atelierf->nb).'</a></td> 
+			<td width="50%"><a href="'._('construire').'-1"><b>'._('Production').' : </b> '.nbf($batis[0]-$constructions->atelierf->nb).'</a></td>
 		</tr>
 		<tr>
-			<td><a href="Construire-2"><b>Militaire : </b> <?php echo nbf($batis[1]-$units[3]); ?></a></td>
-			<td><a href="Construire-3"><b>Logement : </b> <?php echo nbf($batis[2]); ?></a></td>
+			<td><a href="'._('construire').'-2"><b>'._('Militaire').' : </b> '.nbf($batis[1]-$units[3]).'</a></td>
+			<td><a href="'._('construire').'-3"><b>'._('Logement').' : </b> '.nbf($batis[2]).'</a></td>
 		</tr>
 		<tr>
-			<td><a href="Engager-4"><b>Tours : </b> <?php echo nbf($units[3]); ?></a></td>
-			<td><?php
+			<td><a href="'._('engager').'-4"><b>'._('Tours').' : </b> '.nbf($units[3]).'</a></td>
+			<td>';
 			if($units[3] > 0) {
-				echo '<a href="PasserEnRevue"><b>Défense</b> : '.nbf($defense_tours). ' '.img('images/attaques/bouclier.png', 'défense');
+				echo '<a href="'._('passerenrevue').'"><b>'._('Défense').'</b> : '.nbf($defense_tours). ' '.img('images/attaques/bouclier.png', _('défense'));
 			}
 			else {
-				echo '<a href="Construire-4"><b>Autre : </b> '.nbf($batis[3]);
-			} ?></a></td>
+				echo '<a href="'._('construire').'-4"><b>'._('Autre').' : </b> '.nbf($batis[3]);
+			} 
+echo '</a></td>
 		</tr>
 		</tbody>
 		</table>
 		<br/>
 	</div>
 	<div class="ligne">
-	<a class="mtitre" href="PasserEnRevue">Unités</a>
+	<a class="mtitre" href="'._('passerenrevue').'">'._('Unités').'</a>
 	</div>
 	<div class="ligne">
 		<table width="100%" class="none">
 		<tr>
-			<td width="50%"><a href="Engager"><b>Humaines :</b> <?php echo nbf($units[0]+$units[1]); ?></a></td>
-			<td width="50%"><a href="Engager-3"><b>Mythologiques :</b> <?php echo nbf($units[2]); ?></a></td>
+			<td width="50%"><a href="'._('engager').'"><b>'._('Humaines').' :</b> '.nbf($units[0]+$units[1]).'</a></td>
+			<td width="50%"><a href="'._('engager').'-3"><b>'._('Mythologiques').' :</b> '.nbf($units[2]).'</a></td>
 		</tr>
 		<tr>
-			<td><a href="PasserEnRevue"><b>Attaque :</b> <?php echo nbf($attaque).' '.img('images/attaques/dague.png', 'attaque'); ?></a></td>
-			<td><a href="PasserEnRevue"><b>Défense :</b> <?php echo nbf($defense). ' '.img('images/attaques/bouclier.png', 'défense'); ?></a></td>
+			<td><a href="'._('passerenrevue').'"><b>'._('Attaque').' :</b> '.nbf($attaque).' '.img('images/attaques/dague.png', _('attaque')).'</a></td>
+			<td><a href="'._('passerenrevue').'"><b>'._('Défense').' :</b> '.nbf($defense). ' '.img('images/attaques/bouclier.png', _('défense')).'</a></td>
 		</tr>
 		</table>
 		<br/>
 	</div>
 	<div class="ligne">
-	<a href="<?php
+	<a href="';
+
 if($paquet->get_infoj('lvl') == 0) {
-	echo 'Prieres';
+	echo _('Prieres');
 }
 elseif(sizeof($temples) >= 4) {
-	echo 'AutelDesDieux';
+	echo _('AutelDesDieux');
 }
 else {
-	echo 'Temples';
+	echo _('Temples');
 }
 
-echo '" class="mtitre">Mythologie</a></div>
+echo '" class="mtitre">'._('Mythologie').'</a></div>
 	<div class="ligne">';
 
 if(sizeof($temples) > 0) {
@@ -163,52 +165,52 @@ if(sizeof($temples) > 0) {
 		<td width="50%" valign="top">';
 	
 	if(in_array('apollon', $temples)) {
-		echo '<a href="Temples-apollon">'.$temples_donnees['apollon']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('apollon').'">'.$temples_donnees['apollon']['nom'].'</a><br/>';
 	}
 	
 	if(in_array('demeter', $temples)) {
-		echo '<a href="Temples-demeter">'.$temples_donnees['demeter']['nom'].
-		' ('.nbf($paquet->get_infoj('furie')).' <img height=\'10px\' src=\'images/attaques/vignette102.gif\' alt="Furies" title="Furies" />)</a><br/>';
+		echo '<a href="'._('temples').'-'._('demeter').'">'.$temples_donnees['demeter']['nom'].
+		' ('.nbf($paquet->get_infoj('furie')).' <img height=\'10px\' src=\'images/attaques/vignette102.gif\' alt="'._('Furies').'" title="'._('Furies').'" />)</a><br/>';
 	}
 	
 	if(in_array('hermes', $temples)) {
-		echo '<a href="Temples-hermes">'.$temples_donnees['hermes']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('hermes').'">'.$temples_donnees['hermes']['nom'].'</a><br/>';
 	}
 	echo '</td><td width="50%" valign="top">';
 	if(in_array('ares', $temples)) {
-		echo '<a href="Temples-ares">'.$temples_donnees['ares']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('ares').'">'.$temples_donnees['ares']['nom'].'</a><br/>';
 	}
 	
 	if(in_array('athena', $temples)) {
-		echo '<a href="Temples-athena">'.$temples_donnees['athena']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('athena').'">'.$temples_donnees['athena']['nom'].'</a><br/>';
 	}
 	
 	echo '</td></tr>
 	<tr><td valign="top">';
 		
 	if(in_array('artemis', $temples)) {
-		echo '<a href="Temples-artemis">'.$temples_donnees['artemis']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('artemis').'">'.$temples_donnees['artemis']['nom'].'</a><br/>';
 	}
 
 	if(in_array('dionysos', $temples)) {
-		echo '<a href="Temples-dionysos">'.$temples_donnees['dionysos']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('dionysos').'">'.$temples_donnees['dionysos']['nom'].'</a><br/>';
 	}
 	
 	if(in_array('hephaistos', $temples)) {
-		echo '<a href="Temples-hephaistos">'.$temples_donnees['hephaistos']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('hephaistos').'">'.$temples_donnees['hephaistos']['nom'].'</a><br/>';
 	}
 	echo '</td><td valign="top">';
 	if(in_array('zeus', $temples)) {
-		echo '<a href="Temples-zeus">'.$temples_donnees['zeus']['nom'].
-		' ('.nbf($paquet->get_infoj('foudre')).' <img height=\'10px\' src=\'images/attaques/zeus.gif\' alt="Foudres" title="Foudres" />)</a><br/>';
+		echo '<a href="'._('temples').'-'._('zeus').'">'.$temples_donnees['zeus']['nom'].
+		' ('.nbf($paquet->get_infoj('foudre')).' <img height=\'10px\' src=\'images/attaques/zeus.gif\' alt="'._('Foudres').'" title="'._('Foudres').'" />)</a><br/>';
 	}
 	
 	if(in_array('poseidon', $temples)) {
-		echo '<a href="Temples-poseidon">'.$temples_donnees['poseidon']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('poseidon').'">'.$temples_donnees['poseidon']['nom'].'</a><br/>';
 	}
 	
 	if(in_array('hades', $temples)) {
-		echo '<a href="Temples-hades">'.$temples_donnees['hades']['nom'].'</a><br/>';
+		echo '<a href="'._('temples').'-'._('hades').'">'.$temples_donnees['hades']['nom'].'</a><br/>';
 	}
 	echo '</td></tr></table>';
 }
@@ -216,20 +218,20 @@ if(sizeof($temples) > 0) {
 	echo '<table width="100%" class="none">
 	<tr>
 	<td>
-	<a href="arbredesdieux"><b>Arbre des dieux :</b> '.$points_arbre.' points ';
+	<a href="'._('arbredesdieux').'"><b>'._('Arbre des dieux').' :</b> '.$points_arbre.' '._('points').' ';
 	if($points_dispo > 0) {
 		if($points_dispo > 1) {
-			echo ' <span class="ligne90">('.$points_dispo.' disponibles)</span>';
+			echo ' <span class="ligne90">('.$points_dispo.' '._('disponibles').')</span>';
 		}
 		else {
-			echo ' <span class="ligne90">('.$points_dispo.' disponible)</span>';
+			echo ' <span class="ligne90">('.$points_dispo.' '._('disponible').')</span>';
 		}
 	}
 	
 	$nb_bonus = sizeof($paquet->get_infoj('bonus'));
 	
 	echo '</a></td><td>
-	<a href="Bonus"><b>Bonus :</b>  ';
+	<a href="'._('bonus').'"><b>'._('Bonus').' :</b>  ';
 	
 	if($nb_bonus >= 3) {
 		echo '<span class="erreur">'.$nb_bonus.'</span>';
@@ -242,65 +244,67 @@ if(sizeof($temples) > 0) {
 	</td></tr>';
 	if($paquet->get_infoj('lvl') >= 10) {
 		echo '<tr>
-		<td width="50%"><a href="AutelDesDieux"><b>Autels des dieux : </b> '.$niveaux_autels.' '.($niveaux_autels>1?'niveaux':'niveau').'</a></td>
-		<td width="50%"><a href="Statues"><b>Statues :</b> '.$niveaux_arbres.' '.($niveaux_arbres>1?'ornements':'ornement').'</a></td>
+		<td width="50%"><a href="'._('auteldesdieux').'"><b>'._('Autels des dieux').' : </b> '.$niveaux_autels.' '.($niveaux_autels>1?(_('niveaux')):(_('niveau'))).'</a></td>
+		<td width="50%"><a href="'._('statues').'"><b>'._('Statues').' :</b> '.$niveaux_arbres.' '.($niveaux_arbres>1?(_('ornements')):(_('ornement'))).'</a></td>
 		</tr>';
 	}
 	
 	echo '<tr>
-	<td><a href="Succes"><b>Points de succès :</b> '.$points_hf.'</a>
+	<td><a href="'._('succes').'"><b>'._('Points de succès').' :</b> '.$points_hf.'</a>
 	</td>
-	<td><a href="Succes-7"><b>Tours de force :</b> '.$points_tf.'</a>
+	<td><a href="'._('succes').'-7"><b>'._('Tours de force').' :</b> '.$points_tf.'</a>
 	</td>';
 	
-	echo '</table>';
-?><br/>
+	echo '</table>
+	<br/>
 	</div>
-	<div class="ligne mtitre">En cours</div>
+	<div class="ligne mtitre">'._('En cours').'</div>
 	<div class="ligne">
-	<a href="Faveurs"><b>Appui d'Hébé : </b>
-	<?php
+	<a href="'._('faveurs').'"><b>'._('Appui d\'Hébé').' : </b>';
+
 	if(!empty($bonus->tresor) && $bonus->tresor > $paquet->get_timestamp()) {
-		echo 'Actif jusqu\'au '.display_date($bonus->tresor,2);
+		echo _('Actif jusqu\'au').' '.display_date($bonus->tresor,2);
 	}
 	else {
-		echo ' Inactif';
+		echo ' '._('Inactif');
 	}
-	?></a>
+	
+	echo '</a>
 	<br/>
-	<a href="Faveurs"><b>Attaques bonus : </b>
-	<?php
-		echo $bonus->attaque;
-	?></a>
+	<a href="'._('faveurs').'"><b>'._('Attaques bonus').' : </b>'.
+	$bonus->attaque.'</a>
 	<br/>
-	<a href="Faveurs"><b>Appui d'Éros : </b>
-	<?php
+	<a href="'._('faveurs').'"><b>'._('Appui d\'Éros').' : </b>';
+	
 	if(!empty($bonus->xp) && $bonus->xp > $paquet->get_timestamp()) {
-		echo 'Actif jusqu\'au '.display_date($bonus->xp,2);
+		echo _('Actif jusqu\'au').' '.display_date($bonus->xp,2);
 	}
 	else {
-		echo ' Inactif';
+		echo ' '._('Inactif');
 	}
-	?></a>
+	
+	echo '</a>
 	<br/>
-	<a href="Faveurs"><b>Appui d'Hermès : </b>
-	<?php
+	<a href="'._('faveurs').'"><b>'._('Appui d\'Hermès').' : </b>';
+	
 	if(!empty($bonus->commerce) && $bonus->commerce > $paquet->get_infoj('timestamp')) {
-		echo 'Actif jusqu\'au '.display_date($bonus->commerce,2);
+		echo _('Actif jusqu\'au').' '.display_date($bonus->commerce,2);
 	}
 	else {
-		echo ' Inactif';
+		echo ' '._('Inactif');
 	}
-	?></a>
-	<br/><a href="Licences"><b>Licence de grand commerçant : </b>
-	<?php
+	
+	echo '</a>
+	<br/><a href="'._('licences').'"><b>'._('Licence de grand commerçant').' : </b>';
+	
 	if(!empty($bonus->licence) && $bonus->licence > $paquet->get_timestamp()) {
-		echo 'Actif jusqu\'au '.display_date($bonus->licence,2);
+		echo _('Actif jusqu\'au').' '.display_date($bonus->licence,2);
 	}
 	else {
-		echo ' Inactif';
+		echo ' '._('Inactif');
 	}
-	?></a>
+	
+	echo '</a>
 	</div>
 	<div class="clear"></div>
 	</div></div>
@@ -310,13 +314,12 @@ if(sizeof($temples) > 0) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_oracle">
-	<div class="ligne centrer"><a href="oracle" class="titre">Oracle</a></div>
+	<div class="ligne centrer"><a href="'._('oracle').'" class="titre">'.ucfirst(_('oracle')).'</a></div>
 	<div class="ligne centrer">
-	<?php 
-	
-	echo '<a href="NouveauMessage-'.$oracle->id.'" class="gras">Contacter '.$oracle->login.' l\'Oracle</a>';
-	
-	?>
+
+	<a href="'._('nouveaumessage').'-'.$oracle->id.'" 
+	   class="gras">'._('Contacter').' '.$oracle->login.' '._('l\'Oracle').'</a>
+
 	</div>
 	</div></div>
 	<div class="cadre_info_bas"></div>
@@ -325,27 +328,30 @@ if(sizeof($temples) > 0) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_filleuls">
-	<div class="ligne centrer"><a href="Parrainage" class="titre">Filleuls</a></div>
-	<div class="ligne"><?php
+	<div class="ligne centrer"><a href="'._('parrainage').'" class="titre">'._('Filleuls').'</a></div>
+	<div class="ligne">';
+	
 if(sizeof($liste) > 0) {
 	foreach($liste as $do)	{
 		if(!empty($do->timestamp)) {
-			$image='<img src="images/joueurs/mb_connecter.png" alt="Joueur Connecté" />';
+			$image='<img src="images/joueurs/mb_connecter.png" alt="'._('Joueur Connecté').'" />';
 		}
 		else {
-			$image='<img src="images/joueurs/mb_deconnecter.png" alt="Joueur Déconnecté\" />';
+			$image='<img src="images/joueurs/mb_deconnecter.png" alt="'._('Joueur Déconnecté').'" />';
 		}
 		
-		echo $image.' &nbsp;  <a href=\'profilsjoueur-'.$do->id.'\'>'.$do->login.'</a><br/>';
+		echo $image.' &nbsp;  <a href=\''._('profilsjoueur').'-'.$do->id.'\'>'.$do->login.'</a><br/>';
 	}
 }
 elseif($paquet->get_infoj('nb_fil') == 0) {
-	echo '<div class="centrer"><br/><b>Vous n\'avez actuellement aucun filleul</b><br/></div>';
+	echo '<div class="centrer"><br/><b>'._('Vous n\'avez actuellement aucun filleul').'</b><br/></div>';
 }
-?>
+
+echo '
 	</div>
 	<div class="centrer">
-		<a href="Parrainage" class="erreur">Invitez vos amis et gagnez des faveurs !</a><br/>
+		<a href="'._('parrainage').'"
+		   class="erreur">'._('Invitez vos amis et gagnez des faveurs !').'</a><br/>
 	</div>
 	</div></div>
 	<div class="cadre_info_bas"></div>
@@ -356,16 +362,17 @@ elseif($paquet->get_infoj('nb_fil') == 0) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_actu">
-	<div class="ligne centrer"><a href="actualites" class="titre">Actualités</a></div>
-	<div class="ligne">
-<?php
+	<div class="ligne centrer"><a href="'._('actualites').'" class="titre">'._('Actualités').'</a></div>
+	<div class="ligne">';
 
 if(!empty($dernieres_news)) {
 	foreach($dernieres_news as $news) {
-		echo '<div class="news"><a href="'.$news->lien.'" class="titre_news">'.$news->titre.'</a>, <i>le '.date("d-m-y",$news->temps).'</i></div>';
+		echo '<div class="news"><a href="'.$news->lien.'" 
+		                           class="titre_news">'.$news->titre.'</a>, <i>'.display_date($news->temps).'</i></div>';
 	}
 }
-?>
+
+	echo '
 	</div>
 	</div></div>
 	<div class="cadre_info_bas"></div>
@@ -374,17 +381,16 @@ if(!empty($dernieres_news)) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_breves">
-	<div class="ligne centrer"><a href="breves" class="titre">Brèves</a></div>
-	<div class="ligne">
-<?php
+	<div class="ligne centrer"><a href="'._('breves').'" class="titre">'._('Brèves').'</a></div>
+	<div class="ligne">';
 
 if(!empty($dernieres_breves)) {
 	foreach($dernieres_breves as $breves) {
-		echo '<div class="news"><a href="'.$breves->lien.'" class="titre_news">'.$breves->titre.'</a>, <i>le '.date("d-m-y",$breves->temps).'</i></div>';
+		echo '<div class="news"><a href="'.$breves->lien.'" class="titre_news">'.$breves->titre.'</a>, <i>'.display_date($breves->temps).'</i></div>';
 	}
 }
 
-?>
+echo '
 	</div>
 	</div></div>
 	<div class="cadre_info_bas"></div>
@@ -393,15 +399,14 @@ if(!empty($dernieres_breves)) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur" id="cadre_info_archives">
-	<div class="ligne centrer"><a href="Archives" class="titre">Archives</a></div>
-	<div class="ligne">
-	<?php
+	<div class="ligne centrer"><a href="'._('archives').'" class="titre">'._('Archives').'</a></div>
+	<div class="ligne">';
 	
 	foreach($archives as $details) {
 		echo '<div class="news"><a href="" class="titre_news">'.$details->titre.'</a></div>';
 	}
 	
-	?>
+	echo '
 	</div>	
 	</div></div>
 	<div class="cadre_info_bas"></div>
@@ -410,44 +415,45 @@ if(!empty($dernieres_breves)) {
 <div class="cadre_info">
 	<div class="cadre_info_haut"></div>
 	<div class="cadre_info_centre"><div class="cadre_info_interieur">
-	<div class="ligne centrer"><a href="Jeux" class="titre">Jeux</a></div>
+	<div class="ligne centrer"><a href="'._('jeux').'" class="titre">'._('Jeux').'</a></div>
 	<div class="ligne">
 	<table width="100%" class="none">
 		<tr>
 		<td width="50%">
-		<a href="Ticket" class="mtitre">Tickets à gratter</a><br/>
-		<a href="Ticket"><?php
+		<a href="Ticket" class="mtitre">'._('Tickets à gratter').'</a><br/>
+		<a href="Ticket">';
 		if($tikets > 1) {
-			echo '<b>'.$tikets.'</b> tickets restant';
+			echo '<b>'.$tikets.'</b> '._('tickets restant');
 		}
 		else {
-			echo '<b>'.$tikets.'</b> ticket restant';
+			echo '<b>'.$tikets.'</b> '._('ticket restant');
 		}
 		echo '</a></td>
-		<td width="50%"><a href="Loto" class="mtitre">Jouez au loto</a><br/>
-		<a href="Loto">Un ticket gratuit par semaine !</a></td>
+		<td width="50%"><a href="'._('loto').'" class="mtitre">'._('Jouez au loto').'</a><br/>
+		<a href="Loto">'._('Un ticket gratuit par semaine !').'</a></td>
 		</tr>
 		<tr>
 		<td>';
 	if($paquet->get_infoj('lvl') > 1 or sizeof($btn) > 0) {
-		echo '<a href="BataillesNavales" class="mtitre">Batailles navales</a><br/>';
+		echo '<a href="'._('bataillesnavales').'" class="mtitre">'._('Batailles navales').'</a><br/>';
 		if(sizeof($btn) > 0) {
 			foreach($btn as $do) {
 				if(empty($do->titre)) {
-					$do->titre='Partie publique';
+					$do->titre=_('Partie publique');
 				}
 			
 				if(($do->places == 8) && ($do->temps < $paquet->get_timestamp())) {
-					$do->titre='<a href=\'partie-'.$do->id.'\'>'.$do->titre.'</a>';
+					$do->titre='<a href="'._('partie').'-'.$do->id.'">'.$do->titre.'</a>';
 				}
 			
 				echo $do->titre.'<br/>';
 			}
 		}
 	}
-	?></td>
-	<td><a href="CarreMagique" class="mtitre">Carré magique</a><br/>
-	<a href="CarreMagique">Aidez le jeu et remportez des faveurs !</a>
+	
+	echo '</td>
+	<td><a href="'._('carreMagique').'" class="mtitre">'._('Carré magique').'</a><br/>
+	<a href="'._('carreMagique').'">'._('Aidez le jeu et remportez des faveurs !').'</a>
 	</td>
 	</tr>
 	</table>
@@ -456,4 +462,6 @@ if(!empty($dernieres_breves)) {
 	</div></div>
 	<div class="cadre_info_bas"></div>
 </div>
-</div>
+</div>';
+
+?>
