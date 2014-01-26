1aae8299d24e377eb894df54907a246f2c18ec25
Mighty <mightykiller2b@gmail.com>
2014-01-26 23:18:49 +0100

    Lien vers le profil sur la boite de reception

--- include/messagerie.php
+++ include/messagerie.php
@@ -32,7 +32,7 @@ foreach($liste as $mess) {
 	
 echo '</td>
 	<td>&nbsp;<a href="'._('lire').'-'.$mess->id.'">'.$mess->titre.'</a></td>
-	<td>&nbsp;'.$mess->login.'</td>
+	<td>&nbsp;<a href="'._('profilsjoueur').'-'.$mess->destinataire.'">'.$mess->login.'</a></td>
 	<td>'.display_date($mess->date,4).'</td>
 	<td><img src="images/messagerie/supprimer.png"
 	         alt="'._('Supprimer').'"
