2281b3112e73890f3f1455f5c41869d06a27d5bb
Mighty <mightykiller2b@gmail.com>
2014-01-28 23:07:53 +0100

    Ajout du destinataire au click

--- include/nouveaumessage.php
+++ include/nouveaumessage.php
@@ -44,9 +44,11 @@ echo '
 	          placeholder="'._('Message').'"
 	          rows="15" cols="80"/></textarea>
 	<br/><br/>
-	<div class="bouton_classique"><input type="submit" 
+	<div class="bouton_classique"><input type="submit"
+	                                     id="submit"
 	                                     name="changer" 
-	                                     value="'._('Envoyer').'" /></div>
+	                                     value="'._('Envoyer').'"
+	                                     onclick="ajouter_dest()" /></div>
 </div>
 </form>';
 
@@ -63,33 +65,39 @@ $(document).keypress(function(e) {
     }
 });
 
+$("#destinataire").focusout(function() {
+	ajouter_dest();
+});
+
 function ajouter_dest() {
 	
 	var login = $("#destinataire").val();
 	
-  $.ajax({
-    type: "GET",
-    url: "../form/form_mpaddlogin.php",
-    data: "id="+login,
-    success: function(msg) {
-    	
-    	var data = jQuery.parseJSON(msg);
-    	
-      if(data != null && data.id != 0) {
-      	if(jQuery.inArray(data.id, destinataires) == -1) { 
-	      	var txt = '';
-					txt += '<input type="hidden" name="dest[]" id="dest_'+data.id+'" value="'+data.id+'">';
-					txt += data.login + '<br/>';
-	      	
-	      	$("#liste_destinataires").show();
-	      	$("#liste_destinataires").html($("#liste_destinataires").html()+txt);
-	      	destinataires.push(data.id);
-	      	$("#destinataire").removeAttr("required");
-      	}
-      }
-      $("#destinataire").val('');
-      $("#destinataire").focus();
-    }
-  });
+	if(login != '') {
+	  $.ajax({
+	    type: "GET",
+	    url: "../form/form_mpaddlogin.php",
+	    data: "id="+login,
+	    success: function(msg) {
+	    	
+	    	var data = jQuery.parseJSON(msg);
+	    	
+	      if(data != null && data.id != 0) {
+	      	if(jQuery.inArray(data.id, destinataires) == -1) { 
+		      	var txt = '';
+						txt += '<input type="hidden" name="dest[]" id="dest_'+data.id+'" value="'+data.id+'">';
+						txt += data.login + '<br/>';
+		      	
+		      	$("#liste_destinataires").show();
+		      	$("#liste_destinataires").html($("#liste_destinataires").html()+txt);
+		      	destinataires.push(data.id);
+		      	$("#destinataire").removeAttr("required");
+	      	}
+	      }
+	      $("#destinataire").val('');
+	      $("#destinataire").focus();
+	    }
+	  });
+	}
 }
 </script>
