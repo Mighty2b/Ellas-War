00adea648c0f4955e4bbae657a9090cc8cd55530
Mighty <mightykiller2b@gmail.com>
2014-02-09 15:43:56 +0100

    Passage lvl7

--- include/vosobjectifs6.php
+++ include/vosobjectifs6.php
@@ -4,9 +4,11 @@ echo '
 <table class="none">
 <tr>
 	<td>
-	<b>'._('Construction de 35 ateliers de battage de la monnaie').'</b><br/>
-	<i>'._('Actuellement').' : '.$actu[0].' '._(
-	'ateliers de battage de la monnaie').'</i>
+	<b>'._(
+	'Construction de 40 ateliers de battage de la monnaie').
+	'</b><br/>
+	<i>'._('Actuellement').' : '.$actu[0].
+	' '._('ateliers de battage de la monnaie').'</i>
 	</td>
 	<td class="centrer">';
 
@@ -22,9 +24,8 @@ echo '
 </tr>
 <tr>
 	<td>
-	<b>'._('Recrutement de 5\'000 archers à arc long').'</b><br/>
-	<i>'._('Actuellement').' : '.nbf($actu[1]).' '.
-	_('archers à arc long').'</i>
+	<b>'._('Recrutement de 6\'000 hippeis').'</b><br/>
+	<i>'._('Actuellement').' : '.nbf($actu[1]).' '._('hippeis').'</i>
 	</td>
 	<td class="centrer">';
 
@@ -40,24 +41,7 @@ echo '
 </tr>
 <tr>
 	<td>
-	<b>'._('Construction de 250 habitations').'</b><br/>
-	<i>'._('Actuellement').' : '.$actu[2].' '._('habitations').'</i>
-	</td>
-	<td class="centrer">';
-
-	if(!empty($obj[2])) {
-		echo '<b><font color=\'green\'>&nbsp;'._('Objectif accompli').'&nbsp;</font></b>';
-	}
-	else {
-	  echo '<b><font color=\'red\'>&nbsp;'._('Objectif non accompli').'&nbsp;</font></b>';
-	}
-	
-	echo '
-	</td>
-</tr>
-<tr>
-	<td>
-	<b>'._('250 victoires').'</b><br/>
+	<b>'._('350 victoires').'</b><br/>
 	<i>'._('Actuellement').' : '.$actu[3].' victoires
 	</td>
 	<td class="centrer">';
@@ -74,9 +58,38 @@ echo '
 </tr>
 <tr>
 	<td>
+	<b>'._('Construction de votre deuxième temple').'</b><br/>
+	<i>'._('Actuellement :').' '; 
+		if(!empty($obj[2])) {
+			if($actu[2] == 'ares') {
+				echo _('Temple d\'Arès');
+			}
+			else {
+				echo _('Temple d\'Athéna');
+			}
+		}
+		else {
+			echo _('Aucun temple');
+		}
+		
+		echo '</i>
+	</td>
+	<td class="centrer">';
+
+	if(!empty($obj[2])) {
+		echo '<b><font color=\'green\'>&nbsp;'._('Objectif accompli').'&nbsp;</font></b>';
+	}
+	else {
+	  echo '<b><font color=\'red\'>&nbsp;'._('Objectif non accompli').'&nbsp;</font></b>';
+	}
+	
+	echo '
+	</td>
+</tr>
+<tr>
+	<td>
 	<b>'._('Toutes vos ressources en positif').'</b><br/>
-	<i>'._('Actuellement').' : '.
-	((!empty($obj[4]))?_('ressources en positif'):_('au moins une des ressources n\'est pas en positif')).'
+	<i>'._('Actuellement').' : '.((!empty($obj[4]))?_('ressources en positif'):_('au moins une des ressources n\'est pas en positif')).'
 	</td>
 	<td class="centrer">';
 
@@ -94,37 +107,15 @@ echo '
 
 if(array_sum($obj) == sizeof($obj)) {
 	echo '<br/><div class="bouton_classique"><input type="button"
-	                                                value="'._('PASSER NIVEAU 6').'" 
-	                                                onclick="javascript:passer_lvl(5);"/></div>';
+	                                                value="'._('PASSER NIVEAU 7').'" 
+	                                                onclick="javascript:passer_lvl(6);"/></div>';
 }
 
 echo '
-<h2 class="centrer">'._('Après votre passage niveau 6').' :</h2>
+<h2 class="centrer">'._('Après votre passage niveau 7').' :</h2>
 <table class="none">
-<tr>
-	<td>'._(
-	'Nouveau bâtiment disponible : <b>École de cavalerie').
-'</b></td>
-</tr>
 	<tr>
-		<td>'._(
-	'Nouvelle unité disponible : <b>Hippeis</b>').
-'</td>
-	</tr>
-	<tr>
-		<td>'._(
-	'Possibilité de bâtir <b>un nouveau temple</b>').
-'</td>
-	</tr>
-	<tr>
-		<td>'._(
-	'Suppression du plafond de votre <b>trésor</b>').
-'</td>
-	</tr>
-	<tr>
-	  <td>'._(
-	'Appui d\'<b>Hébé</b>').
-'</td>
+		<td>'._('Nouvelle unité disponible : <b>Archer à cheval</b>').'</td>
 	</tr>
 </table>
 </center>';
