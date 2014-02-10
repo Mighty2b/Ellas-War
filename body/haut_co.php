2f8cc03a0f5bc8b54ff56cb2b5b369d05a69ce1f
Mighty <mightykiller2b@gmail.com>
2014-02-10 20:51:56 +0100

    lien par défaut vers les unités

--- body/haut_co.php
+++ body/haut_co.php
@@ -86,7 +86,7 @@ echo '
 </section>
 <section id="header_menu">
 &nbsp;<a id="menu_cite" href="'._('cite').'"><div class="ssmenu">'._('Cité').'</div>'._('Cité').'</a>&nbsp;
-&nbsp;<a id="menu_armee" href="'._('armee').'"><div class="ssmenu">'._('Armée').'</div>'._('Armée').'</a>&nbsp;
+&nbsp;<a id="menu_armee" href="'._('passerenrevue').'"><div class="ssmenu">'._('Armée').'</div>'._('Armée').'</a>&nbsp;
 &nbsp;<a id="menu_archives" href="'._('archives').'"><div class="ssmenu">'.ucfirst(_('archives')).'</div>'.ucfirst(_('archives')).'</a>&nbsp;
 &nbsp;<a id="menu_mythologie" href="';
 
