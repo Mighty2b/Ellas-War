4b002778d12c7884f0c604ea95f19a5a975c1677
Mighty <mightykiller2b@gmail.com>
2014-02-22 14:55:04 +0100

    Nouvelles pages

--- include/classements.php
+++ include/classements.php
@@ -4,35 +4,35 @@ echo '<h2 class="centrer">'._('Classements').'</h2>
       <br/><br/>';
 
 echo '<a href="'._('classementdesjoueurs').'"
-         class="gras">'._('Classement des joueurs').'</a><br/>';
-echo 'Classement des cités d\'Ellàs War, celui-ci se fait '.
+         class="gras">'._('Classement des joueurs').'</a><br/>'.
+   _('Classement des cités d\'Ellàs War, celui-ci se fait '.
      'par rapport au niveau du joueur puis grâce à un système d\'XP. '.
-     'L\'XP augmente lors d\'une victoire et baisse lors d\'une défaite.';
+     'L\'XP augmente lors d\'une victoire et baisse lors d\'une défaite.');
 
 echo '<br/>
       <br/>
       <a href="'._('classementdesalliances').'"
-         class="gras">'._('Classement des Alliances').'</a><br/>';
-echo 'Classement des alliances d\'Ellàs War, celui-ci se fait '.
+         class="gras">'._('Classement des Alliances').'</a><br/>'.
+   _('Classement des alliances d\'Ellàs War, celui-ci se fait '.
      'par rapport au niveau de l\'alliance puis grâce à un système d\'XP. '.
      'Par leurs attaques les membres font gagner ou perdre de l\'XP à leur '.
-     'alliance. Il est aussi possible de gagner ou perdre de l\'XP grâce aux guerres.';
+     'alliance. Il est aussi possible de gagner ou perdre de l\'XP grâce aux guerres.');
 
 echo '<br/>
       <br/>
       <a href="'._('classementdesbtn').'"
-         class="gras">'._('Classement des Batailles navales').'</a><br/>';
-echo 'Les batailles navales sont une guerre maritime entre quatre joueurs. '.
+         class="gras">'._('Classement des Batailles navales').'</a><br/>'.
+   _('Les batailles navales sont une guerre maritime entre quatre joueurs. '.
      'Leur score est calculé sur un dérivé du ELO qui est à celui utilisé '.
-     'pour classer les joueurs d\'echec.';
+     'pour classer les joueurs d\'echec.');
 
 echo '<br/>
       <br/>
       <a href="'._('classementdesheros').'"
-         class="gras">'._('Classement des Héros').'</a><br/>';
-echo 'Chaque cité d\'ELlàs War peut engager un héros. Celui-ci '.
+         class="gras">'._('Classement des Héros').'</a><br/>'.
+   _('Chaque cité d\'ELlàs War peut engager un héros. Celui-ci '.
      'peut être envoyé faire des quêtes ou attaquer les héros '.
-     'des autres cités.';
+     'des autres cités.');
 
 
 ?>
---
+++ include/communaute.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<h1>'._('Communauté d\'Ellàs War').'</h1><br/><br/>';
+
+echo '<div class="ligne justify">'.
+   _('La communauté d\'Ellàs War se retrouve sur le forum. Vous pourrez '.
+     'y trouver de l\'aide et communiquer avec les autres joueurs. '.
+     'Si vous le souhaitez vous pouvez faire une demande et disposer d\''.
+     'une ou plusieurs parties pour vous et votre alliance.').'<br/>'.
+    _('Régulierement les joueurs se connectent le soir sur Teamspeak '.
+      'pour participer à des jeux et remporter des prix.').'</div>';
+
+?>
