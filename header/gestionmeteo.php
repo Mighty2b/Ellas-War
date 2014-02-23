3a45f3804a201005d1daf5418103ddf8e8c1a827
Mighty <mightykiller2b@gmail.com>
2014-02-23 23:29:21 +0100

    mauvais header

--- header/gestionmeteo.php
+++ header/gestionmeteo.php
@@ -5,10 +5,10 @@ echo '<title>'._('Gestion de la météo').'</title>
 
 if(!empty($_GET['var1'])) {
 	if($_GET['var1'] == 1) {
-		$paquet = new EwPaquet('anti_meteo');
+		$paquet -> add_action('anti_meteo');
 	}
 	else {
-		$paquet = new EwPaquet('annuler_anti_meteo');
+		$paquet -> add_action('annuler_anti_meteo');
 	}
 }
 
