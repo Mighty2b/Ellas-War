239027354ed84825249761ca785fb94bd71dabdf
Mighty <mightykiller2b@gmail.com>
2014-03-16 12:33:20 +0100

    Fix de l'erreur lorsque le client et serveur ne sont plus en liaison

--- form/chat_joueurs.php
+++ form/chat_joueurs.php
@@ -6,32 +6,35 @@ $paquet = new EwPaquet();
 $paquet -> add_action('joueurs_chat');
 $paquet -> send_actions();
 
-$rep = $paquet->get_answer('joueurs_chat')->{1};
-$me  = false;
+if(!empty($paquet->get_answer('joueurs_chat'))) {
+	$rep = $paquet->get_answer('joueurs_chat')->{1};
 
-if(!empty($rep)) {
-	foreach($rep as $j) {
-		if($j->id == $paquet->get_infoj('id')) {
-			$me = true;
+	$me  = false;
+	
+	if(!empty($rep)) {
+		foreach($rep as $j) {
+			if($j->id == $paquet->get_infoj('id')) {
+				$me = true;
+			}
+			
+			echo '<b><a href="'._('profilsjoueur').'-'.$j->id.'"
+									target="_blank"
+									'.($j->admin?'class="erreur"':'').'>'.$j->login.'</a></b><br/>';
 		}
-		
-		echo '<b><a href="'._('profilsjoueur').'-'.$j->id.'"
-								target="_blank"
-								'.($j->admin?'class="erreur"':'').'>'.$j->login.'</a></b><br/>';
 	}
-}
-
-if($me) {
-echo '<script type="text/javascript">
-	$("#cadre_chat_iconeco").attr("src", "images/utils/mb_connecter.png");
-	$("#cadre_chat_iconedeco").show();
-</script>';
-}
-else {
-echo '<script type="text/javascript">
-	$("#cadre_chat_iconeco").attr("src", "images/utils/mb_deconnecter.png");
-	$("#cadre_chat_iconedeco").hide();
-</script>';
+	
+	if($me) {
+	echo '<script type="text/javascript">
+		$("#cadre_chat_iconeco").attr("src", "images/utils/mb_connecter.png");
+		$("#cadre_chat_iconedeco").show();
+	</script>';
+	}
+	else {
+	echo '<script type="text/javascript">
+		$("#cadre_chat_iconeco").attr("src", "images/utils/mb_deconnecter.png");
+		$("#cadre_chat_iconedeco").hide();
+	</script>';
+	}
 }
 
 ?>
