0ca5db9ee633064ebcc14eacfd53ed27f13c6c84
Mighty <mightykiller2b@gmail.com>
2014-03-18 22:49:16 +0100

    textes des poursuites

--- header/poursuites.php
+++ header/poursuites.php
@@ -5,4 +5,6 @@ echo '
 <meta name="description"
       content="'._('Poursuites').'" />';
 
+$paquet->add_action('stats_tmp');
+
 ?>
---
+++ include/poursuites.php
@@ -0,0 +1,33 @@
+<?php
+
+echo '<h2 class="centrer">'._('Poursuites').'</h2>';
+
+echo '<div class="ligne_80">
+<div class="ligne">
+	<div class="ligne_50">
+	<b>'._('Joueur heureux').'</b><br/>';
+	printf(_('Gagnez au moins %s en jouant aux jeux'), nbf(10000));
+	echo '<br/><br/></div>
+	<div class="ligne_50 centrer">Mission en cours</div>
+</div>
+<div class="ligne">
+	<div class="ligne_50"><b>' ._('Visiteur d\'ailleurs').'</b><br/>
+	'._('Visiter le forum').'<br/><br/></div>
+	<div class="ligne_50 centrer">Mission en cours</div>
+</div>
+
+<div class="ligne">
+	<div class="ligne_50"><b>' ._('Combattant du dimanche').'</b><br/>
+	'._('Effectuer au moins 4 attaques hors guerre').'<br/><br/></div>
+	<div class="ligne_50 centrer">Mission en cours</div>
+</div>
+
+<div class="ligne">
+	<div class="ligne_50"><b>' ._('Je gratte, tu grattes...').'</b><br/>
+	'._('Gratter au moins 10 tickets').'<br/><br/></div>
+	<div class="ligne_50 centrer">Mission en cours</div>
+</div>
+
+</div>';
+
+?>
