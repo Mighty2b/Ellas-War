df1d0f2b5d28db84e5806de7719268aec7fed902
Mighty <mightykiller2b@gmail.com>
2014-03-22 13:52:26 +0100

    Poursuites

--- include/poursuites.php
+++ include/poursuites.php
@@ -10,9 +10,10 @@ echo '<div class="ligne_80">
 <div class="ligne">
 	<div class="ligne_50">
 	<b>'._('Joueur heureux').'</b><br/>';
-	printf(_('Gagnez au moins %s en jouant aux jeux'), 
+	printf(_('Gagner au moins %s en jouant aux jeux'), 
 	       nbf(10000). ' '.imress('drachme'));
-	echo '<br/><br/></div>
+	echo '<br/><i>Récompense :</i> 2 500 '.imress('drachme').'
+	<br/><br/></div>
 	<div class="ligne_50 centrer"><br/>';
 
 if($paquet->get_answer('stats_tmp')->{1}->jeux_valide == 0) {
@@ -22,7 +23,8 @@ if($paquet->get_answer('stats_tmp')->{1}->jeux_valide == 0) {
 		     imress('drachme');
 	}
 	else {
-		echo _('Valider la mission');
+		echo '<div class="cursor rouge_goco"
+		           onclick="valider_mission(\'joueur_heureux\', 1)">'._('Valider la mission').'</div>';
 	}
 }
 else {
@@ -33,14 +35,21 @@ echo '</div>
 </div>
 <div class="ligne">
 	<div class="ligne_50"><b>' ._('Visiteur d\'ailleurs').'</b><br/>
-	'._('Visiter le forum').'<br/><br/></div>
+	'._('Visiter le forum').
+	'<br/><i>Récompense :</i> 2 500 '.imress('drachme').'
+	<br/><br/></div>
 	<div class="ligne_50 centrer"><br/>';
 
 if($paquet->get_answer('stats_tmp')->{1}->forum_visite == 0) {
-	echo '<a href="'.FORUM_URL.'" target="_blank">'._('Visiter le forum').'</a>';
+	echo '<a href="'.FORUM_URL.'"
+	         onclick="valider_mission(\'visiteur_ailleurs\', 0)"
+	         class="rouge_goco"
+	         target="_blank">'._('Visiter le forum').'</a>';
 }
 else {
-	echo '<span class="rouge_goco">'._('Mission validée').'</span>';
+	echo '<span class="rouge_goco">'._('Mission validée').
+	     ' (<a href="'.FORUM_URL.'"
+	           target="_blank">'._('Visiter le forum').'</a>)</span>';
 }
 
 echo '</div>
@@ -48,7 +57,9 @@ echo '</div>
 
 <div class="ligne">
 	<div class="ligne_50"><b>' ._('Combattant du dimanche').'</b><br/>
-	'._('Effectuer au moins 4 attaques hors guerre').'<br/><br/></div>
+	'._('Effectuer au moins 4 attaques hors guerre').
+	'<br/><i>Récompense :</i> 20 000 '.imress('drachme').'
+	<br/><br/></div>
 	<div class="ligne_50 centrer"><br/>';
 
 if($paquet->get_answer('stats_tmp')->{1}->attaque_valide == 0) {
@@ -57,7 +68,9 @@ if($paquet->get_answer('stats_tmp')->{1}->attaque_valide == 0) {
 		     nbf($paquet->get_answer('stats_tmp')->{1}->attaque_hg);
 	}
 	else {
-		echo _('Valider la mission');
+		echo '<a href="#"
+		         class="cursor rouge_goco"
+		         onclick="valider_mission(\'combattant_dimanche\', 1)">'._('Valider la mission').'</a>';
 	}
 }
 else {
@@ -69,7 +82,9 @@ echo '</div>
 
 <div class="ligne">
 	<div class="ligne_50"><b>' ._('Je gratte, tu grattes...').'</b><br/>
-	'._('Gratter au moins 10 tickets').'<br/><br/></div>
+	'._('Gratter au moins 10 tickets').
+	'<br/><i>Récompense :</i> 20 000 '.imress('drachme').'
+	<br/><br/></div>
 	<div class="ligne_50 centrer"><br/>';
 
 if($paquet->get_answer('stats_tmp')->{1}->tickets_valide == 0) {
@@ -78,7 +93,9 @@ if($paquet->get_answer('stats_tmp')->{1}->tickets_valide == 0) {
 		     nbf($paquet->get_answer('stats_tmp')->{1}->tickets_gratte);
 	}
 	else {
-		echo _('Valider la mission');
+		echo '<a href="#"
+		         class="cursor rouge_goco"
+		         onclick="valider_mission(\'jetu_gratte\', 1)">'._('Valider la mission').'</a>';
 	}
 }
 else {
@@ -88,6 +105,21 @@ else {
 echo '</div>
 </div>
 
-</div>';
+</div>
+
+<script type="text/javascript">
+
+function valider_mission(mission, hide) {
+   $.ajax({
+     type: "GET",
+     url: "form/valide_quetej.php",
+     data: "mission="+mission,
+     success: function(msg){
+     		window.location.href = \''._('poursuites').'\';
+     }
+   });
+}
+
+</script>';
 
 ?>
