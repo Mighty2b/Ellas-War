0fe3b20a2932f06a6af0bf93d0635508d88b7b98
Mighty <mightykiller2b@gmail.com>
2014-05-06 20:07:11 +0200

    Message quand le debarras est vide

--- form/debarras_reload.php
+++ form/debarras_reload.php
@@ -8,32 +8,37 @@ $paquet -> send_actions();
 
 $liste = $paquet->get_answer('liste_debarras')->{1};
 
-echo '<table><thead><tr class="centrer">
-<td></td>
-<td>Taux</td>
-<td>Quantité</td>
-<td class="none"></td>
-</tr></thead><tfoot></tfoot><tbody>';
-$i = 1;
-foreach($liste as $lot) {
-	echo '<tr>
-	      <td onclick="set_ress('.$i.', '.$lot->restant.')"
-	          class="cursor">'.nbf($lot->restant).' '.imress($lot->ressource).'</td>
-	      <td>'.nbf($lot->taux,6).'</td>
-	      <td><input type="text"
-	                 name="lot_'.$lot->ressource.'"
-	                 placeholder="0"
-	                 id="lot_'.$i.'" /></td>
-	      <td><img alt="'._('Prendre').'"
-	               title="'._('Prendre').'" 
-	               src="images/com/cart_add.png"
-	               class="cursor"
-	               onclick="debarras_acheter(\''.$lot->ressource.'\',
-	                                         '.$i.', '.$lot->taux.')"></td>
-	      </tr>';
-	$i++;
+if(!empty($liste) && sizeof($liste) > 0) {
+	echo '<table><thead><tr class="centrer">
+	<td></td>
+	<td>Taux</td>
+	<td>Quantité</td>
+	<td class="none"></td>
+	</tr></thead><tfoot></tfoot><tbody>';
+	$i = 1;
+	foreach($liste as $lot) {
+		echo '<tr>
+		      <td onclick="set_ress('.$i.', '.$lot->restant.')"
+		          class="cursor">'.nbf($lot->restant).' '.imress($lot->ressource).'</td>
+		      <td>'.nbf($lot->taux,6).'</td>
+		      <td><input type="text"
+		                 name="lot_'.$lot->ressource.'"
+		                 placeholder="0"
+		                 id="lot_'.$i.'" /></td>
+		      <td><img alt="'._('Prendre').'"
+		               title="'._('Prendre').'" 
+		               src="images/com/cart_add.png"
+		               class="cursor"
+		               onclick="debarras_acheter(\''.$lot->ressource.'\',
+		                                         '.$i.', '.$lot->taux.')"></td>
+		      </tr>';
+		$i++;
+	}
+	
+	echo '</tbody></table>';
 }
-
-echo '</tbody></table>';
+else {
+	echo '<div class="centrer gras">'._('Le débarras est vide').'</div>';
+}
 
 ?>
