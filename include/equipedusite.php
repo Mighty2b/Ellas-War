32528d2a455fdb1d604a2c2072db68c711886748
Mighty <mighty@ellaswar.com>
2014-07-13 23:43:33 +0200

    Liens dans des nouvelles pages

--- include/equipedusite.php
+++ include/equipedusite.php
@@ -4,7 +4,7 @@ echo '<h1>'._('L\'équipe d\'Ellàs War').'</h1>
 <br/><div class="ligne justify">'._(
 'Si vous pensez avoir les qualités requises pour rejoindre l\'équipe '.
 'n\'hésitez pas à <a href="nouscontacter">nous contacter</a>. '.
-'Vous pouvez le faire soit en utilisant <a href="'.FORUM_URL.'">notre forum</a>, '.
+'Vous pouvez le faire soit en utilisant <a href="'.FORUM_URL.'" target="_blank">notre forum</a>, '.
 'soit par la <a href="nouscontacter">page contact</a>. '.
 'La liste des <a href="recrutement">compétences que nous recherchons</a> '.
 'n\'est pas exaustive et nous répondons individuellement à toutes les demandes.').
@@ -14,21 +14,21 @@ echo '<h1>'._('L\'équipe d\'Ellàs War').'</h1>
 <br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Administrateurs').' :</b> Mighty
 <br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Prophètes').' : </b> Mspeedy78, Kassie
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Prophètes').' : </b> Mspeedy78
 <br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Héros').' :</b> MAXXX29
 <br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Graphismes').' :</b> Goco, Alex07654
 <br/>
 <br/>'._(
-'Remerciements à <a href="http://www.flogisoft.com/">Flozz</a> pour son jeu "la Chasse aux trésors".').'
+'Remerciements à <a href="http://www.flogisoft.com" target="_blank">Flozz</a> pour son jeu "la Chasse aux trésors".').'
 
 <br/><br/>
 <i>'._('Remerciment à nos anciens').' :</i>
 <br/>
 <div class="gauche">
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Administrateurs').' : </b> Notperfect, Rayz, Octaviux, Dreamer, Goco<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Prophètes').' : </b> Nico11570, Aminouch, Thomast, Melpomene, Missmaude, Driguer, Letnev, Grimmjow, Babyangel85, Lespoune, Legendel, Gilgam3sh<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Prophètes').' : </b> Nico11570, Aminouch, Thomast, Melpomene, Missmaude, Driguer, Letnev, Grimmjow, Babyangel85, Lespoune, Legendel, Gilgam3sh, Kassie<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Modérateurs').' :</b> Alexmax, Callysta, Stephrgr, Volvic, Pinpinbleu, Akenagan, Lunch, Odysseus, kazenver, Steen, Padommay, Electhor<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>'._('Graphistes').' : </b> Galbatorix, Dreamer, Rastaking
 <br/><br/>
