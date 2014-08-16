527aaf086aba6a82079257747d592b2f1fb03004
Mighty <mightykiller2b@gmail.com>
2014-08-16 09:59:17 +0200

    Les partenaires sont spécifiques à une langue

--- include/partenaires.php
+++ include/partenaires.php
@@ -17,42 +17,8 @@ echo '<h1>'.ucfirst(_('partenaires')).'</h1>
 <img src="banmini.gif" alt="Ellàs War" title="Ellàs War" /><br/>
 <textarea class="part_banniere"><a href="'.SITE_URL.'" target="_blank"><img src="'.SITE_URL.'/banmini.gif" alt="Ellàs War" /></a></textarea>';
 
+include('lang/'.LANG.'/partenaires.php');
+
 ?>
 
-<br/><br/>
-
-<a href="http://www.argent-gratuit.net" 
-   target="_blank"><img src="http://www.argent-gratuit.net/bouton.gif" 
-                        width="88" 
-                        height="31" 
-                        alt="argent-gratuit"/></a>
-
-<a href="http://www.cadomax.com" 
-   target="_blank" ><img src="http://www.cadomax.com/design/8831-cadomax-01.gif" border="0" width="88" height="31" alt="jeux" title="jeux"></a>
-
-<a href="http://www.clicjeux.net" 
-   target="_blank"><img src="http://www.clicjeux.net/images/bouton.gif" 
-                        alt="clicjeux.net" 
-                        border="0"></a>
-<br/>
-<a href="http://www.gamersroom.com/vote.php?num=1150" 
-   title="Gamers'room - Annuaire des jeux gratuit par internet (pbem)" 
-   target="_blank"><img src="http://www.gamersroom.com/pub/bouton4.gif" 
-                        alt="Gamers'room" /></a>
-
-<a href="http://www.medieval-war.com/index.php?id=65" 
-   target="_blank"><img src="http://www.medieval-war.com/banniere2.gif" 
-                        alt="medieval-war"/></a>
-
-<a href="http://www.meilleursjeuxvirtuels.com" 
-   target="_blank"><img src="http://www.meilleursjeuxvirtuels.com/images/gif88x31.gif" 
-                        alt="meilleursjeuxvirtuels" 
-                        title="Meilleurs Jeux Virtuels" /></a>
-
-<a href="http://www.sitacados.com"
-   target="_blank"><img src="http://www.sitacados.com/images/sitacados-8831.gif" 
-                        width="88" 
-                        height="31" 
-                        alt="siteacados" /></a>
-<br/>
 </div>
---
+++ lang/fr_FR/partenaires.php
@@ -0,0 +1,32 @@
+<br/><br/>
+
+<a href="http://www.argent-gratuit.net" 
+   target="_blank"><img src="http://www.argent-gratuit.net/bouton.gif" 
+                        width="88" 
+                        height="31" 
+                        alt="argent-gratuit"/></a>
+
+<a href="http://www.cadomax.com" 
+   target="_blank" ><img src="http://www.cadomax.com/design/8831-cadomax-01.gif" border="0" width="88" height="31" alt="jeux" title="jeux"></a>
+<br/>
+
+<a href="http://www.gamersroom.com/vote.php?num=1150" 
+   title="Gamers'room - Annuaire des jeux gratuit par internet (pbem)" 
+   target="_blank"><img src="http://www.gamersroom.com/pub/bouton4.gif" 
+                        alt="Gamers'room" /></a>
+
+<a href="http://www.medieval-war.com/index.php?id=65" 
+   target="_blank"><img src="http://www.medieval-war.com/banniere2.gif" 
+                        alt="medieval-war"/></a>
+
+<a href="http://www.meilleursjeuxvirtuels.com" 
+   target="_blank"><img src="http://www.meilleursjeuxvirtuels.com/images/gif88x31.gif" 
+                        alt="meilleursjeuxvirtuels" 
+                        title="Meilleurs Jeux Virtuels" /></a>
+
+<a href="http://www.sitacados.com"
+   target="_blank"><img src="http://www.sitacados.com/images/sitacados-8831.gif" 
+                        width="88" 
+                        height="31" 
+                        alt="siteacados" /></a>
+<br/>
