93c1dfc85732a4033facf0007ef7b407c47b8032
Mighty <mightykiller2b@gmail.com>
2014-08-19 20:24:51 +0200

    Fix annulation des guerres

--- header/guerres.php
+++ header/guerres.php
@@ -7,8 +7,8 @@ echo '<title>'._('Les guerres de votre alliance').'</title>
 if(!empty($_POST['choix'])) {
 	$paquet -> add_action('action_ultimatum', array($_POST['id'], $_POST['choix']));
 }
-elseif(!empty($_GET['var1']) && !empty($_GET['var2']) && $_GET['var1'] == 'annuler') {
-	$paquet -> add_action('annuler_guerre', $_GET['var2']);
+elseif(!empty($_GET['var1']) && !empty($_GET['var2']) && $_GET['var1'] == _('annuler')) {
+	$paquet -> add_action('annuler_guerre', array($_GET['var2']));
 }
 
 $paquet -> add_action('info_mes_guerres');
