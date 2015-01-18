4214b91457356bb57fb766e6612ae62a6b432a13
Mighty <mightykiller2b@gmail.com>
2015-01-18 11:58:16 +0100

    Preparation de la maj 5.2

---
+++ index2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>EllasWar.com revient bientôt !</title>
+	<meta http-equiv="refresh" content="60; url=http://forums.ellaswar.com" />
+</head>
+<body bgcolor="#000000">
+	<center><a href="http://forums.ellaswar.com"><img src="images/illustration/ewguerre.jpg" border="0"></a></center>
+	<object type="audio/mpeg" data="guerre.mp3" height="0" width="0">
+		<param name="filename" value="guerre.mp3" />
+		<param name="autostart" value="true" />
+		<param name="loop" value="true" />
+	</object>
+</body>
+</html>
