ec348e2fab773e0fe78b73c2943ca378a67a983a
Mighty <mightykiller2b@gmail.com>
2015-02-03 20:10:31 +0100

    Le css passe dans le fichier dédié

--- lang/fr_FR/ress_deco.php
+++ lang/fr_FR/ress_deco.php
@@ -1,4 +1,4 @@
-<div style="position:relative;width:88px;height:31px;margin-left:35px;margin-bottom:3px;"><a href="http://www.jeu-gratuit.net/jeux-de-strategie-mmorts/" target="_blank"><img src="http://www.jeu-gratuit.net/images/bannieres/88x31_general_1.png" alt="jeux gratuits : Jeux de stratégie & MMORPG"  width="88" height="31" /></a><a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/#jeux" target="_blank" style="position:absolute; top:3px; left:1px; font-family:arial; font-size:11px; font-weight:bold; color:#ffffff; text-decoration:none; line-height:12px; width:86px; text-align:center;">Jeux gratuits<br/>de stratégie</a></div>
+<div class="jeu-gratuit"><a href="http://www.jeu-gratuit.net/jeux-de-strategie-mmorts/" target="_blank"><img src="http://www.jeu-gratuit.net/images/bannieres/88x31_general_1.png" alt="jeux gratuits : Jeux de stratégie & MMORPG"  width="88" height="31" /></a><a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/#jeux" target="_blank" class="jeu-gratuit2">Jeux gratuits<br/>de stratégie</a></div>
 
 
 <a href="http://www.jeu-gratuit.net/#jeux" 
@@ -13,9 +13,9 @@
     micropolia_echban_height  = 31;
     micropolia_echban_jeu     = 11221;
 </script>
-<script src="http://www.jeu-gratuit.net/tracking/affiche.js" type="text/javascript"></script>
+<script src="http://www.jeu-gratuit.net/tracking/affiche.js" type="text/javascript" async="async"></script>
 
-<div style="position:relative;width:88px;height:31px;margin-left:35px;margin-bottom:3px;">
+<div class="jeu-gratuit">
 <a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/jeu-ellas-war.html" 
    target="_blank"><img src="http://www.jeu-gratuit.net/images/bannieres/88x31_general_1.png" 
                         alt="votez pour Ellas War" 
@@ -27,13 +27,10 @@
 
 <a href="http://www.jeu-gratuit.net/jeux-de-strategie-et-jeux-de-role/jeu-ellas-war.html#voter" 
    target="_blank" 
-   style="position:absolute; top:8px; left:-1px; font-family:arial; font-size:11px; font-weight:bold; color:#ffffff; text-decoration:none; line-height:12px; width:90px; text-align:center;" 
+   class="jeu-gratuit3"
    title="votez pour Ellas War">Votez ici !!</a></div>
 
 
 <a href="http://www.jeux-alternatifs.com/Ellas-War-jeu601_hit-parade_1_1.html"
    target="_blank"><img src="http://www.jeux-alternatifs.com/im/bandeau/hitP_88x31_v1.gif" 
                         alt="Voter pour Ellàs War" /></a>
-<br/>
-<a href="http://www.hannuaire.fr/" 
-   title="Annuaire référencement gratuit"><img src="http://www.hannuaire.fr/i/bleu.png" alt="Annuaire web" style="border:0"/></a>
