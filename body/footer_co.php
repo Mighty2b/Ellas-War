87c0d3ff53e4b62e7b735bf7606fb92a79fd297a
Mighty <mightykiller2b@gmail.com>
2015-07-19 15:14:08 +0200

    Fix du chargement des anciens messages

--- body/footer_co.php
+++ body/footer_co.php
@@ -151,7 +151,7 @@ socket.on(\'chatUser\', function(users) {
 socket.on(\'chatMsg\', function(chat) {
 	$("#corps_chat").html(\'\');
 	for(var i = chat.length-1; i >= 0; i--) {
-		$("#corps_chat").html(\'<b>\'+chat[i].login+\' :</b>\'+chat[i].msg+\'<br/>\');
+		$("#corps_chat").html($("#corps_chat").html()+\'<b>\'+chat[i].login+\' :</b>\'+chat[i].msg+\'<br/>\');
 	}
 });
 </script>';
